55577d8a453ed4c3fb8a5d117355d45a52e103e6
Jeremy Smith <jmsmith57@hotmail.com>
2016-11-27 16:00:59 +0000

    order of name, type and hp can be reversed and ordered, changed from ul and li to a table for the displaying of data

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -2,14 +2,30 @@ angular
   .module("ZombieEnd")
   .controller("GameController", GameController)
 
-function GameController($scope, $http) {
-  
-  $http.get("../zombies.json")
-  .success(function(data, status, headers, config) {
-  $scope.zombies = data;
-  }).
-  error(function(data, status, headers, config) {
+  function GameController($scope, $http) {
 
-  });
+    $http.get("../zombies.json")
+      .success(function(data, status, headers, config) {
+      $scope.zombies = data;
+      }).
+      error(function(data, status, headers, config) {
+
+      });
+
+    $scope.header ='name';
+    $scope.reverse = true;
+
+
+    $scope.sortBy = function(header){
+      $scope.reverse = ($scope.header === header) ? ! $scope.reverse : false;
+      $scope.header = header;
+    };
+    
+    $scope.shotsFired = 0
+
+    $scope.shoot = function(){
+    console.log("shots fired");
+    $scope.shotsFired +=1
+  }
 
 } 
