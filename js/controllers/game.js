74a38f34bf071618c208c2b730993a59daf89905
Jeremy Smith <jmsmith57@hotmail.com>
2016-11-27 16:41:30 +0000

    game and win logic nearly finished just have to iron out a few errors and do the timeouts

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -2,7 +2,10 @@ angular
   .module("ZombieEnd")
   .controller("GameController", GameController)
 
-  function GameController($scope, $http) {
+  function GameController($scope, $http, $state, $timeout) {
+
+    $scope.zombies = []
+    $scope.zombiesKilled = []
 
     $http.get("../zombies.json")
       .success(function(data, status, headers, config) {
@@ -24,8 +27,30 @@ angular
     $scope.shotsFired = 0
 
     $scope.shoot = function(){
-    console.log("shots fired");
-    $scope.shotsFired +=1
-  }
+      console.log("shots fired");
+      $scope.shotsFired +=1
 
+      var randomNumber = Math.floor(Math.random()* $scope.zombies.length)
+      var zombieTarget = $scope.zombies[randomNumber]
+      zombieTarget.hp -= zombieTarget.damage
+
+      var hpLeft = 100/zombieTarget.maxHp*zombieTarget.hp
+      if (hpLeft < 70 && hpLeft >= 40) {
+        document.getElementById(zombieTarget.name).className="orange";
+      }else if (hpLeft < 40 && hpLeft >= 1){
+        document.getElementById(zombieTarget.name).className="red";
+      }
+      if (zombieTarget.hp <= 0) {
+        zombieTarget.hp = 0
+        $scope.zombiesKilled.push(zombieTarget);
+        $scope.zombies.splice(randomNumber, 1);
+        $scope.checkWinner();
+      }
+      
+    }
+    $scope.checkWinner = function(){
+      if ($scope.zombies.length === 0 || zombieTarget.name === "MASTER ZOMBIE") {
+        $state.go("winner");
+      }
+    }
 } 
