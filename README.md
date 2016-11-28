435510c4e0b5a8a322f738f8e87f150a95094031
Jeremy Smith <jmsmith57@hotmail.com>
2016-11-28 08:22:01 +0000

    made some minor tweaks, and tidied up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # end-zombie
+
+Please use $ python -m SimpleHTTPServer 8000
--- js/controllers/game.js
+++ js/controllers/game.js
@@ -4,68 +4,64 @@ angular
 
   function GameController($scope, $http, $state, $timeout) {
 
-    $scope.zombies = []
+  $scope.zombies = []
+  $scope.zombiesKilled = []
+
+  $scope.getZombies = function (){
+  $http.get("../zombies.json")
+    .success(function(data, status, headers, config) {
+    $scope.zombies = data;
+    }).
+    error(function(data, status, headers, config) {
+
+    });
     $scope.zombiesKilled = []
-
-    $scope.getZombies = function (){
-    $http.get("../zombies.json")
-      .success(function(data, status, headers, config) {
-      $scope.zombies = data;
-      }).
-      error(function(data, status, headers, config) {
-
-      });
-      $scope.zombiesKilled = []
-      $scope.shotsFired = 0
-      $timeout.cancel($scope.timeout);
-    }
-
-    $scope.header ='name';
-    $scope.reverse = true;
-
-
-    $scope.sortBy = function(header){
-      $scope.reverse = ($scope.header === header) ? ! $scope.reverse : false;
-      $scope.header = header;
-    };
-    
     $scope.shotsFired = 0
+    $timeout.cancel($scope.timeout);
+  }
 
-    var zombieTarget
+  $scope.header ='name';
+  $scope.reverse = true;
 
-    $scope.shoot = function(){
-      console.log("shots fired");
-      $scope.shotsFired +=1
 
-      var randomNumber = Math.floor(Math.random()* $scope.zombies.length)
-      zombieTarget = $scope.zombies[randomNumber]
-      zombieTarget.hp -= zombieTarget.damage
+  $scope.sortBy = function(header){
+    $scope.reverse = ($scope.header === header) ? ! $scope.reverse : false;
+    $scope.header = header;
+  };
+  
+  $scope.shotsFired = 0
 
-      var hpLeft = 100/zombieTarget.maxHp*zombieTarget.hp
-      if (hpLeft < 70 && hpLeft >= 40) {
-        document.getElementById(zombieTarget.name).className="orange";
-      }else if (hpLeft < 40 && hpLeft >= 1){
-        document.getElementById(zombieTarget.name).className="red";
-      }
-      if (zombieTarget.hp <= 0 ) {
-        zombieTarget.hp = 0
-        $scope.zombiesKilled.push(zombieTarget)
-        $scope.zombies.splice(randomNumber, 1)
-        $scope.checkWinner();
-      }
-      
+  var zombieTarget
+
+  $scope.shoot = function(){
+    console.log("shots fired");
+    $scope.shotsFired +=1
+
+    var randomNumber = Math.floor(Math.random()* $scope.zombies.length)
+    zombieTarget = $scope.zombies[randomNumber]
+    zombieTarget.hp -= zombieTarget.damage
+
+    var hpLeft = 100/zombieTarget.maxHp*zombieTarget.hp
+    if (hpLeft < 70 && hpLeft >= 40) {
+      document.getElementById(zombieTarget.name).className="orange";
+    }else if (hpLeft < 40 && hpLeft >= 1){
+      document.getElementById(zombieTarget.name).className="red";
     }
-    $scope.checkWinner = function(){
-      if ($scope.zombies.length === 0 || zombieTarget.name === "MASTER ZOMBIE") {
-        $state.go("winner");
-        $scope.timeout = $timeout(function(){
-          $state.go("home");
-        }, 10000)
-      }
-
+    if (zombieTarget.hp <= 0 ) {
+      zombieTarget.hp = 0
+      $scope.zombiesKilled.push(zombieTarget)
+      $scope.zombies.splice(randomNumber, 1)
+      $scope.checkWinner();
+    }
+    
+  }
+  $scope.checkWinner = function(){
+    if ($scope.zombies.length === 0 || zombieTarget.name === "MASTER ZOMBIE") {
+      $state.go("winner");
+      $scope.timeout = $timeout(function(){
+        $state.go("home");
+      }, 10000)
     }
 
-
-
-
+  }
 } 
